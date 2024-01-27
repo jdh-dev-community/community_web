8fcae7d3b5babbe5a219478deb9a79fb6092c5e8
Jo JaeHoon <dev.hoony@gmail.com>
2024-01-27 12:28:48 +0900

    게시글 비밀번호 입력 및 카테고리 입력 개선 (#7)

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -6,14 +6,26 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  try {
+    const response = await createPost(req.body);
+
+    console.log("response", response);
+    // res.redirect(307, `/post/${id}`);
+  } catch (err) {
+    console.log("err", err);
+
+    throw new Error("Failed to submit the data. Please try again.");
+  }
+}
+
+const createPost = async (body: any) => {
   const response = await fetch("http://3.36.204.107/api/v1/post", {
-    method: req.method,
-    body: req.body,
+    method: "POST",
+    body: body,
     headers: {
       "Content-Type": "application/json",
     },
   });
 
-  console.log("status", response.status);
-  res.redirect(201, "/");
-}
+  return response;
+};
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -9,13 +9,20 @@ export default function PostPage() {
       <input type="text" name="creator" />
 
       <div>카테고리</div>
-      <input type="text" name="category" />
+      <select name="category" className="text-black">
+        <option value="질문">질문</option>
+        <option value="홍보">홍보</option>
+        <option value="상담">질문</option>
+      </select>
 
       <div>제목</div>
       <input type="text" name="title" />
 
       <div>내용</div>
       <input type="text" name="content" />
+
+      <div>비밀번호</div>
+      <input type="text" name="password" />
 
       <button type="submit">Submit</button>
     </form>
