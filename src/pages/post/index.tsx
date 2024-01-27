2e595b1b32eccd6c60cf6d1a40779e618fa1306f
Jo JaeHoon <dev.hoony@gmail.com>
2024-01-27 14:21:54 +0900

    글 생성 시 항목 필요 조건 적용 (#15)

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -6,23 +6,31 @@ export default function PostPage() {
   return (
     <form onSubmit={onSubmit}>
       <div>닉네임</div>
-      <input type="text" name="creator" />
+      <input type="text" name="creator" required />
 
       <div>카테고리</div>
       <select name="category" className="text-black">
-        <option value="concern">고민</option>
+        <option value="concern" selected>
+          고민
+        </option>
         <option value="question">질문</option>
         <option value="daily">일상</option>
       </select>
 
       <div>제목</div>
-      <input type="text" name="title" />
+      <input type="text" name="title" required />
 
       <div>내용</div>
-      <input type="text" name="content" />
+      <input type="text" name="content" required />
 
       <div>비밀번호</div>
-      <input type="text" name="password" />
+      <input
+        type="text"
+        name="password"
+        pattern="\S{4,}"
+        placeholder="4자 이상 입력해주세요"
+        required
+      />
 
       <button type="submit">Submit</button>
     </form>
