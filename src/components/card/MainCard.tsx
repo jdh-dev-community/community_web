598bd6d77b508941f0f15b060db287b1ff35c663
devsince2021 <dev.ryu2021@gmail.com>
2024-03-12 19:17:42 +0900

    qa: 카드에서 컨텐츠 최대 사이즈 반영

--- src/components/card/MainCard.tsx
+++ src/components/card/MainCard.tsx
@@ -79,7 +79,7 @@ export const MainCard = ({
             {title}
           </h4>
         </div>
-        <div className=" text-gray-800 text-base mb-4 line-clamp-3">
+        <div className=" text-gray-800 text-base mb-4 line-clamp-3 max-h-[40px]">
           <div dangerouslySetInnerHTML={{ __html: content }} />
         </div>
       </div>
